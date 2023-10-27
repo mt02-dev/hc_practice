86b2bc469932e5825b51e28c3d72ef9a03b75c53
mt02-dev <t.m.215soccer@icloud.com>
2023-10-27 21:06:36 +0900

    グループ分け用フラグを修正し、条件分岐が発生しなように修正

--- ruby/group.rb
+++ ruby/group.rb
@@ -1,13 +1,9 @@
 group = ['A', 'B', 'C', 'D', 'E', 'F']
 
 shuffled = group.shuffle
-flag = [0, 1].shuffle
+flag = [3, 4].shuffle
 
-if flag[0].zero?
-  divided = shuffled.each_slice(3).to_a
-else
-  divided = shuffled.each_slice(4).to_a
-end
+divided = shuffled.each_slice(flag[0]).to_a
 
 p divided[0].sort
 p divided[1].sort
