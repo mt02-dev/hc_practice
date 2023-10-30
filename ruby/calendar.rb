b7ca4d499a4cf395a860b47fd30d483f7319033e
mt02-dev <t.m.215soccer@icloud.com>
2023-10-31 07:27:36 +0900

    条件分岐、不要なスペース、空行を削除

--- ruby/calendar.rb
+++ ruby/calendar.rb
@@ -1,48 +1,41 @@
 require 'optparse'
 require 'date'
 
+def print_header(date)
+  month_year_header = date.strftime('%B %Y')
+  weeks_header = %w[月 火 水 木 金 土 日].join(' ')
+  puts "#{month_year_header.center(20)}\n#{weeks_header}"
+end
+
 def print_calender(month)
   year = Date.today.year
-
-  first_day = Date.new(year,month,1)
-  last_day = Date.new(year,month,-1)
+  first_day = Date.new(year, month, 1)
+  last_day = Date.new(year, month, -1)
   days_num = last_day.mday
   days_array = []
 
-  if first_day.sunday?
-    (1..6).each { days_array << '  '}
-  end
-  
+  6.each { days_array << '  ' } if first_day.sunday?
   unless first_day.monday? || first_day.sunday?
-    (1..first_day.wday - 1 ).each { days_array << '  ' }
+    (1..first_day.wday - 1).each { days_array << '  ' }
   end
 
-  (1..days_num).each do |i|
-    days_array << i.to_s.rjust(2,' ')
-  end 
-  month_year_header = first_day.strftime("%B %Y")
-  weeks_header = %w[月 火 水 木 金 土 日].join(' ')
-  puts "#{month_year_header.center(20)}\n#{weeks_header}"
-  
-  days_array.each.with_index(1) do | day, index |
-    if day == days_array[-1] 
+  (1..days_num).each { |i| days_array << i.to_s.rjust(2, ' ') }
+  print_header(first_day)
+
+  days_array.each.with_index(1) do |day, index|
+    if day == days_array[-1]
       puts day
-    elsif index % 7 == 0
+    elsif (index % 7).zero?
       puts day
-    else 
+    else
       print "#{day} "
     end
   end
-
 end
 
 def num_validation(num_str)
   num = num_str.to_i
-  if num < 1 || 12 < num
-    0
-  else
-    num
-  end
+  num < 1 || num > 12 ? 0 : num
 end
 
 def print_calender_no_opt
@@ -51,16 +44,16 @@ end
 
 opt = OptionParser.new
 
-opt.on('-m') {
+opt.on('-m') do
   result = num_validation(ARGV[0])
   if result.nonzero?
     print_calender(result)
   else
     puts "#{ARGV[0]} is neither a month number (1..12) nor a name"
-  end 
-}
+  end
+end
 
-if ARGV.size.zero?
+if ARGV.empty?
   print_calender_no_opt
 else
   opt.parse!(ARGV)
