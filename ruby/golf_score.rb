503c3402a6d4c4b3c926385aba9bc3ce748d2fd8
mt02-dev <t.m.215soccer@icloud.com>
2023-11-02 12:25:56 +0900

    スコア判定の判定方法を修正

--- ruby/golf_score.rb
+++ ruby/golf_score.rb
@@ -1,6 +1,12 @@
-SCORE_NAME_OVER_ZERO = %w[パー ボギー]
-SCORE_NAME_UNDER_ZERO = %w[コンドル アルバトロス イーグル バーディ]
 HOLE_IN_ONE = 'ホールインワン'
+SCORE_MAPPING = {
+  1 => "ボギー",
+  0 => "パー",
+  -1 => "バーディ",
+  -2 => "イーグル",
+  -3 => "アルバトロス",
+  -4 => "コンドル"
+}
 
 score_data = readlines(chomp: true)
 the_number_of_strokes_each_hole = score_data[0].split(',').map(&:to_i)
@@ -17,17 +23,11 @@ difference_in_scores.each_with_index do |score, index|
     next
   end
 
-  case score
-  when -1, -2, -3, -4
-    results << SCORE_NAME_UNDER_ZERO[score]
-  when 0
-    results << SCORE_NAME_OVER_ZERO[score]
-  when 1..nil
-    if score > 1
-      results << "#{score}#{SCORE_NAME_OVER_ZERO[1]}"
-      next
-    end
-    results << SCORE_NAME_OVER_ZERO[1]
+  if score > 1
+    results << "#{score}#{SCORE_MAPPING[1]}"
+    next
   end
+
+  results << SCORE_MAPPING[score]
 end
 puts results.join(',')
